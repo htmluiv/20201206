8d2d6cd0248cf15d40055ae224972b96dc27a919
htmluiv <73861559+htmluiv@users.noreply.github.com>
2020-12-06 22:11:54 +0900

    create time slider
    create time slider
    코로나 발생 이후 몇일의 시간이 흘렀는가 - 시간의 흐름 표시

--- sketch.js
+++ sketch.js
@@ -1,8 +1,15 @@
 function setup() {
   createCanvas(400, 400);
+  slider = createSlider(1, 999, 2);
+  timepass = 0;
 }
 
 function draw() {
-  background(220);
-  rect(40,40,40,40);
+  background(0);
+  textStyle(BOLD);
+  textSize(20);
+  fill(255, 0, 0);
+  text(timepass+ floor(slider.value()), 0, 20);
+  fill(110);
+  text('Days After COVID-19 Emerges', 40, 20);
 }
