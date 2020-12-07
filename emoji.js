ec8dce5c182235205adcd05ddf034fd340307b62
htmluiv <73861559+htmluiv@users.noreply.github.com>
2020-12-07 15:42:48 +0900

    마우스의 위치에 따른 이모티콘 생성
    마우스의 위치에 따른 이모티콘 생성

--- emoji.js
+++ emoji.js
@@ -3,6 +3,10 @@ class Emoji  {
     strokeWeight(4);
     stroke(0);
     fill(255, 180, 0);
-    ellipse(mouseX, mouseY, 60,60);
+    ellipse(mouseX, mouseY, 80,80);
+    line(mouseX-20, mouseY-20, mouseX-10, mouseY-10);
+    line(mouseX+20, mouseY-20, mouseX+10, mouseY-10);
+    line(mouseX-15, mouseY+25, mouseX, mouseY+17);
+    line(mouseX+15, mouseY+25, mouseX, mouseY+17);
   }
 }
