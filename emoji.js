22c14f1daabf1ab07b0a4456aa398a1cbd909698
htmluiv <73861559+htmluiv@users.noreply.github.com>
2020-12-07 15:51:07 +0900

    슬라이더에 따른 눈과 입의 각도 변화
    슬라이더에 따른 눈과 입의 각도 변화
    - 시간이 지날수록 눈의 각도는 내려가고, 입의 각도는 올라간다 [미소]

--- emoji.js
+++ emoji.js
@@ -4,9 +4,9 @@ class Emoji  {
     stroke(0);
     fill(255, 180, 0);
     ellipse(mouseX, mouseY, 80,80);
-    line(mouseX-20, mouseY-20, mouseX-10, mouseY-10);
-    line(mouseX+20, mouseY-20, mouseX+10, mouseY-10);
-    line(mouseX-15, mouseY+25, mouseX, mouseY+17);
-    line(mouseX+15, mouseY+25, mouseX, mouseY+17);
+    line(mouseX-20, mouseY-20+slider.value()/100, mouseX-10, mouseY-10); //left eye
+    line(mouseX+20, mouseY-20+slider.value()/100, mouseX+10, mouseY-10); //rignt eye
+    line(mouseX-15, mouseY+25-slider.value()/70, mouseX, mouseY+17); // left lip
+    line(mouseX+15, mouseY+25-slider.value()/70, mouseX, mouseY+17); // right lip
   }
 }
