530b8b70315dd728e2c7db240f31f57fc07960d1
htmluiv <73861559+htmluiv@users.noreply.github.com>
2020-12-15 18:29:46 +0900

    설명 추가
    설명 추가

--- sketch.js
+++ sketch.js
@@ -10,6 +10,13 @@ function setup() {
   slider = createSlider(1, 999, 2);
   timepass = 0;
   repeller = new Repeller();
+
+  createP('"US"를 잡고 움직여 바이러스로부터 도망치세요.');
+  createP('슬라이더를 움직여보세요.');
+  createP('1. 코로나 바이러스의 출현으로 부터 얼마나 지났는지 확인하세요.');
+  createP('2. 시간이 지남에 따른 바이러스의 개수를 확인하세요.');
+  createP('3. 시간이 지남에 따른 얼굴의 변화를 확인하세요.');
+  createP('4. 시간이 지남에 따른 분위기의 변화를 확인하세요.')
 }
 
 function draw() {
